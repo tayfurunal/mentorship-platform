0dc5346a3b6eb848782278a7d3a7ec08d9ed0d98
tayfurunal <mtayfurunal@gmail.com>
2019-11-16 16:14:12 +0300

    first commit

---
+++ kanban-backend/src/test/java/com/tayfurunal/kanbantool/KanbantoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tayfurunal.kanbantool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbantoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
